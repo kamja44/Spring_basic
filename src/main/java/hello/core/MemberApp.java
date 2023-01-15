98976de4bf85b5f68b64c88d1408abcedb81ba3a
kamja44 <88872409+kamja44@users.noreply.github.com>
2023-01-15 23:28:52 +0900

    기본 자바 코드 -> 스프링

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,11 +4,19 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        // String은 항상 ApplicationContext로 시작한다.(ApplicationContext = String Container
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        // Appconfig에 있는 설정파일들(@Bean)을 스프링 컨테이너 안에서 관리한다.
+
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
         Member findMember = memberService.findMember(1L);
